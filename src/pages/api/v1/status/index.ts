67cd937ec89d9d87ff51ffde3c131710de31940e
Geison Oriani <geison.oriani@outlook.com>
2024-02-12 14:59:56 -0300

    Add `database.ts` and `.env` files

--- src/pages/api/v1/status/index.ts
+++ src/pages/api/v1/status/index.ts
@@ -1,12 +1,14 @@
 /* Dependencies */
 import type { NextApiRequest, NextApiResponse } from 'next'
+import database from '../../../../../infra/database'
 
 /* Project */
 import { ResponseData } from './types'
 
-export default function handler(
-	_req: NextApiRequest,
-	res: NextApiResponse<ResponseData>,
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
 ) {
-	return res.status(200).send({ message: 'ok' })
+  console.log('Database:', await database.query())
+  return res.status(200).send({ message: 'ok' })
 }
