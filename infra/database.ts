e5d12008415393cd8ec023d6799bac701f5acfae
Geison Oriani <geison.oriani@outlook.com>
2024-02-13 11:08:26 -0300

    Make `database.ts` more robust to error handling

--- infra/database.ts
+++ infra/database.ts
@@ -1,20 +1,24 @@
 import { Client } from 'pg'
 
-async function query() {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-  await client.connect()
-  const res = await client.query('SELECT 1 + 1 as result')
-  await client.end()
-
-  return res
+export async function createClient() {
+	const client = new Client({
+		host: process.env.POSTGRES_HOST,
+		port: Number(process.env.POSTGRES_PORT),
+		database: process.env.POSTGRES_DB,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+	})
+	return client
 }
 
-export default {
-  query,
+export async function query(sql: string) {
+	const client = await createClient()
+	await client.connect()
+	try {
+		return await client.query(sql)
+	} catch (error) {
+		console.error(error)
+	} finally {
+		await client.end()
+	}
 }
